6081451948d2247a0b13dcabde8fe379264b1e3d
weipch <weipch5152@aliyun.com>
2024-03-10 10:03:01 +0800

    feat：死信队列消息被拒

--- rabbitmq-study/src/main/java/com/weipch/rabbitmq/dlq/Consumer02.java
+++ rabbitmq-study/src/main/java/com/weipch/rabbitmq/dlq/Consumer02.java
@@ -15,20 +15,12 @@ import java.util.Map;
  */
 public class Consumer02 {
 
-
-	private static final String NORMAL_EXCHANGE = "normal_exchange";
-	private static final String DEAD_EXCHANGE = "dead_exchange";
-
-	private static final String NORMAL_QUEUE = "normal_queue";
 	private static final String DEAD_QUEUE = "dead_queue";
 
 
 	public static void main(String[] args) throws Exception {
 		Channel channel = RabbitMqUtils.getChannel();
-		//声明死信交换机和队列
-		channel.exchangeDeclare(DEAD_EXCHANGE, BuiltinExchangeType.DIRECT);
-		channel.queueDeclare(DEAD_QUEUE, false, false, false, null);
-		channel.basicConsume(NORMAL_QUEUE, true,
+		channel.basicConsume(DEAD_QUEUE, true,
 			(consumerTag, delivery) -> System.out.println("Consumer02:" + new String(delivery.getBody(), StandardCharsets.UTF_8)),
 			(consumerTag, e) -> {});
 	}
