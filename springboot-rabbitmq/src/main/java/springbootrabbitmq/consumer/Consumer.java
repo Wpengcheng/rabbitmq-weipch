cc32dfdf64369b985e3f2dd87238712c571d4340
weipch <weipch5152@aliyun.com>
2024-03-10 13:59:54 +0800

    feat：延迟队列

---
+++ springboot-rabbitmq/src/main/java/springbootrabbitmq/consumer/Consumer.java
@@ -0,0 +1,18 @@
+package springbootrabbitmq.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+import springbootrabbitmq.config.ConfirmConfig;
+
+
+@Slf4j
+@Component
+public class Consumer {
+    @RabbitListener(queues = ConfirmConfig.CONFIRM_QUEUE_NAME)
+    public void receiveConfirmMessage(Message message) {
+        String msg = new String(message.getBody());
+        log.info("收到到的队列confirm.queue消息：{}",msg);
+    }
+}
---
+++ springboot-rabbitmq/src/main/java/springbootrabbitmq/consumer/WarningConsumer.java
@@ -0,0 +1,19 @@
+package springbootrabbitmq.consumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+import springbootrabbitmq.config.ConfirmConfig;
+
+
+@Component
+@Slf4j
+public class WarningConsumer {
+    //    接收报警消息
+    @RabbitListener(queues = ConfirmConfig.WARNING_QUEUE_NAME)
+    public void receiveWarningMsg(Message message) {
+        String msg = new String(message.getBody());
+        log.error("报警发现不可路由消息：{}", msg);
+    }
+}
